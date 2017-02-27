2eb4c32f47458d4ec6ed5c1071e6e08c3c20e097
Chris Lamb <lamb.chrisr@gmail.com>
2017-02-27 13:29:41 -0800

    Make call to api and render results in the view.

--- adoptMe/src/App.js
+++ adoptMe/src/App.js
@@ -1,9 +1,45 @@
 import React from 'react';
+import credentials from './credentials'
+import petfinder from './petfinder-client'
+const pf = petfinder(credentials)
 
 class App extends React.Component {
+  constructor(props) {
+      super(props)
+
+      this.state = {
+        animal: 'dog',
+        breed: 'Boxer',
+        location: 'St Petersburg, FL',
+        pets: []
+      }
+  }
+
+  componentDidMount() {
+    // This pulls the variable off state and assigns it.
+    // Equivalent to:
+    // const animal = this.state.animal
+    const { animal, breed, location } = this.state
+    // This is ES6 and converts it to animal: animal
+    const promise = pf.pet.find({animal, breed, location, output: 'full'})
+    // Error function does not create a new context.
+    promise.then((data) => {
+      const pets = data.petfinder.pets ? data.petfinder.pets.pet : []
+      this.setState({pets})
+    })
+
+  }
+
   render() {
     return(
-      <h1>SUCCESS</h1>
+      <div className='app'>
+        <img src='images/adopt-me.png' alt='adopt-me logo' />
+        <div>
+          <pre><code>
+            {JSON.stringify(this.state, null, 4)}
+          </code></pre>
+        </div>
+      </div>
     )
   }
 }
