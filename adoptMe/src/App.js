474fa213d6d493fcddbc45ee0baaabc011d5776f
Chris Lamb <lamb.chrisr@gmail.com>
2017-02-27 14:46:41 -0800

    Add search controls (that do not work yet).

--- adoptMe/src/App.js
+++ adoptMe/src/App.js
@@ -2,21 +2,28 @@ import React from 'react';
 import credentials from './credentials'
 import petfinder from './petfinder-client'
 import Pet from './Pet'
+import SearchControls from './SearchControls'
 const pf = petfinder(credentials)
 
 class App extends React.Component {
   constructor(props) {
-      super(props)
+    super(props)
 
-      this.state = {
-        animal: 'dog',
-        breed: 'Mastiff',
-        location: 'St Petersburg, FL',
-        pets: []
-      }
+    this.state = {
+      animal: 'dog',
+      breed: 'Mastiff',
+      location: 'St Petersburg, FL',
+      pets: []
+    }
+
+    this.changeBreed = this.changeBreed.bind(this)
   }
 
   componentDidMount() {
+    this.search()
+  }
+
+  search() {
     // This pulls the variable off state and assigns it.
     // Equivalent to:
     // const animal = this.state.animal
@@ -28,13 +35,21 @@ class App extends React.Component {
       const pets = data.petfinder.pets ? data.petfinder.pets.pet : []
       this.setState({pets})
     })
+  }
 
+  changeBreed(breed) {
+    this.setState({breed})
   }
 
   render() {
     return(
       <div className='app'>
         <img src='adopt-me.png' alt='adopt-me logo' />
+        <SearchControls
+          breed={this.state.breed}
+          animal ={this.state.animal}
+          changeBreed={this.changeBreed}
+        />
         <div>
           {this.state.pets.map((pet) => {
             return (
