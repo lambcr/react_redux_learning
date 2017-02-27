adf771da9ee15c5fde989afc2a09e15a3b79de99
Chris Lamb <lamb.chrisr@gmail.com>
2017-02-27 11:56:27 -0800

    Use our own App and files.

--- adoptMe/src/index.js
+++ adoptMe/src/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './reset.css'
+import './style.css'
 
 ReactDOM.render(
   <App />,
